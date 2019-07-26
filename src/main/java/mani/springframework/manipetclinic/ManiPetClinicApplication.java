aa2bc832ee60e59cd8c0b08b29278cfacf9363ed
lokendramani <lokendramani@gmail.com>
2019-07-26 22:47:52 +0530

    Initial project add

---
+++ src/main/java/mani/springframework/manipetclinic/ManiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mani.springframework.manipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManiPetClinicApplication.class, args);
+    }
+
+}
