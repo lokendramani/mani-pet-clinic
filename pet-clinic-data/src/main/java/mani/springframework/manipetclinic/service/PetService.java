0e414321efac07a91060c990fe8a4b230c9eaf23
lokendramani <lokendramani@gmail.com>
2019-07-28 22:34:25 +0530

    Implemented Map based services. Closes #6

--- pet-clinic-data/src/main/java/mani/springframework/manipetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/mani/springframework/manipetclinic/service/PetService.java
@@ -2,6 +2,6 @@ package mani.springframework.manipetclinic.service;
 
 import mani.springframework.manipetclinic.model.Pet;
 
-public interface PetService  extends CrudService<Pet,Long>{
+public interface PetService  extends CrudService<Pet,Long> {
 
 }
