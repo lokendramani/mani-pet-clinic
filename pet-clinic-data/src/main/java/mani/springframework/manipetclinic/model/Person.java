2b2a46eabf5e82a09216715ac847d09c1b1dba66
lokendramani <lokendramani@gmail.com>
2019-07-28 13:54:38 +0530

    Adding BaseEntity. Closes #10

--- pet-clinic-data/src/main/java/mani/springframework/manipetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/mani/springframework/manipetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package mani.springframework.manipetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/mani/springframework/manipetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/mani/springframework/manipetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package mani.springframework.manipetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
