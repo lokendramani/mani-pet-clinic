20c7884e3d926b4fb817c59785648a34fb9c491a
lokendramani <lokendramani@gmail.com>
2019-08-10 23:26:44 +0530

    Added Visit class. updated owner with Pet relationship Closes #26

---
+++ pet-clinic-data/src/main/java/mani/springframework/manipetclinic/model/Visit.java
@@ -0,0 +1,36 @@
+package mani.springframework.manipetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
