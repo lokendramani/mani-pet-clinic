4ed52fccc48a27cc964253499ccc29fffbfa63d7
lokendramani <lokendramani@gmail.com>
2019-08-11 22:11:35 +0530

    Creates Specialty Map service Impl .Closes #31

---
+++ pet-clinic-data/src/main/java/mani/springframework/manipetclinic/service/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package mani.springframework.manipetclinic.service;
+
+import mani.springframework.manipetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/mani/springframework/manipetclinic/service/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package mani.springframework.manipetclinic.service.map;
+
+import mani.springframework.manipetclinic.model.Speciality;
+import mani.springframework.manipetclinic.service.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
